a36f4eefa7ad6033919f5bcb44140a38a4e261ee
AsmusRise <asris24@student.sdu.dk>
2026-05-07 18:12:11 +0200

    Revert "revert?"
    This reverts commit 9aae7dc68f708f0557b0a70787c17d42c5155569.

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 #include "queue.h"
 #include "semphr.h"
 #include "color_led.h"
+#include "status_led.h"
 #include "encoder.h"
 #include "key.h"
 #include "logger.h"
@@ -43,6 +44,7 @@ static void setupHardware(void)
   // TODO: Put hardware configuration and initialisation in here
 
   // Warning: If you do not initialize the hardware clock, the timings will be inaccurate
+  status_led_init();
   init_systick();
   led_init();
   uart0_init(9600, 8, 1, 'n');
